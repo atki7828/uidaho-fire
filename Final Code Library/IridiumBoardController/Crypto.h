0bf777442f2c968e3ba9c6838f8387dab28ac462
Brandon Foss <46940941+FossBrandon@users.noreply.github.com>
2020-05-06 01:48:02 -0700

    Improved code readability
    moved the max line length to 110 to improve readability

--- Final Code Library/IridiumBoardController/Crypto.h
+++ Final Code Library/IridiumBoardController/Crypto.h
@@ -3,22 +3,18 @@
 // Functionality: Library to handle encryption/decryption of messages.
 // Notes:
 // Adapted by Brandon Foss from https://github.com/kokke/tiny-AES-c
-// Encryption and Decryption is AES-128 bit utilizing CBC mode of operation.
+// Encryption and Decryption is AES-128 bit utilizing the CBC mode of operation.
 // The Message must be padded to an interval of 16 bytes by appending ^'s onto the end of a message.
 // The encryption utilizes a 16 byte (128 bit) symmetric key and IV (initialization vector).
 
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
-// Libraries
+// Libraries:
 #include <string.h>
-//#include <cstdint>
 #include <stdint.h>
-//#include <cstdio>
 #include <stdio.h>
-//#include <cstring>
 #include <string.h>
-//#include <cstdlib>
 #include <stdlib.h>
 #include <Arduino.h>
 
@@ -29,8 +25,8 @@
 #define NB 4 // Defines the number of columns comprising a state in AES
 #define NK 4 // Defines the number of 32 bit words in a key
 #define NR 10 // Defines the number of rounds in AES the cipher
-#define getSBoxValue(num) (sbox[(num)]) // Defines method to retrieve substitution box value
-#define getSBoxInvert(num) (rsbox[(num)]) // Defines method to retrieve the inverse substitution box value
+#define getSBoxValue(num) (sbox[(num)]) // Retrieves substitution box value
+#define getSBoxInvert(num) (rsbox[(num)]) // Retrieves the inverse substitution box value
 // Defines method used to multiply numbers in the field GF(2^8)
 #define Multiply(x, y)                                \
       (  ((y & 1) * x) ^                              \
@@ -45,15 +41,16 @@ class Crypto
     public:
         // Methods:
         Crypto();
-        void init(); // Called to initialize all variables
         uint8_t* encrypt_cbc(String str);
         uint8_t* decrypt_cbc(uint8_t* str);
         int compareArrays(uint8_t arr1[], uint8_t arr2[], int size);
         
     private:
         // Variables:
-        uint8_t key[16] = { 0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x6b, 0x65, 0x79 }; // Key is 16 Bytes (128 bits)
-        uint8_t iv[16]  = { 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76 }; // Initialization vector is 16 Bytes (128 bits)
+        uint8_t key[16] = { 0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x74, 0x65, 0x73, 0x74, 
+            0x20, 0x6b, 0x65, 0x79 }; // Key is 16 Bytes (128 bits)
+        uint8_t iv[16]  = { 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x70, 0x71, 0x72, 
+            0x73, 0x74, 0x75, 0x76 }; // Initialization vector is 16 Bytes (128 bits)
         typedef uint8_t state_t[4][4];  
 
         // Methods
